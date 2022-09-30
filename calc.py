686a98376a032579e6746dbd138fb9a6c89c37f0
Aman Gupta <106864241+Aamangupta70077@users.noreply.github.com>
2022-10-01 00:50:23 +0530

    aman changes

--- calc.py
+++ calc.py
@@ -16,6 +16,13 @@ def multiply(x, y):
 def divide(x, y):
     return x / y
 
+    def multiply(x, y):
+    return x * y
+
+# This function divides two numbers
+def divide(x, y):
+    return x / y
+
 
 print("Select operation.")
 print("1.Add")
