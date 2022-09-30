b7939f3b68782bee98a100361741dd47c7d159d5
Ankul Rathour <ankulnvs007@gmail.com>
2022-10-01 00:22:15 +0530

    please add for ankul

--- calc.py
+++ calc.py
@@ -41,6 +41,19 @@ print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
 
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+
+print("Select operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
 while True:
     # take input from the user
     choice = input("Enter choice(1/2/3/4): ")
